2da67f2c93f6b81604bde2c326fd1efc29a9a0b9
Kashim Mirza11234 <kashimmirza86@gmail.com>
2024-09-24 02:09:15 +0600

    Services folder has been deleted

--- Services/IPaymentService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Emedicine.Services
-{
-    public interface IPaymentService
-    {
-        Task<string> ProcessCreditCardPaymentAsync(decimal amount, string token);
-        Task<string> ProcessBkashPaymentAsync(decimal amount, string phoneNumber);
-        Task<string> ProcessRocketPaymentAsync(decimal amount, string phoneNumber);
-    }
-}
--- Services/ITokenService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Emedicine.Services
-{
-    public interface ITokenService
-    {
-        Task<string> GenerateTokenAsync(string phoneNumber, string email);
-        Task<bool> VerifyTokenAsync(string token, string phoneNumber, string email);
-    }
-}
